2cb5c3932415e1660100b437ea0ad21091adce7f
Stanisław Kiraga <kiragasta@gmail.com>
2023-02-16 09:17:52 +0100

    Add loop which sum values

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ shopping_list = {
 }
 for shop, products in shopping_list.items():
     print('Idę do', shop.capitalize(), 'i kupuję tam', [product.capitalize() for product in products])
-     
+
+sum = 0
+for value in shopping_list.values():
+    sum += len(value)
+print(f'W sumie kupuję {sum} produktów.')     
